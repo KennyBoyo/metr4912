bed79aa96197c8bca4187635042d62e157b8b039
Jack McNamara <jack.mcnamara@uq.edu.au>
2023-03-07 12:14:41 +1000

    remove push and pull scripts

--- pull.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-USER_ID='JunKhai'
-USER_PW='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'
-
-origin=$(git remote get-url origin)
-origin_with_pass=${origin/"//"/"//${USER_ID}:${USER_PW}@"}
-git pull ${origin_with_pass} master
--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-USER_ID='JunKhai'
-USER_PW='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'
-
-origin=$(git remote get-url origin)
-origin_with_pass=${origin/"//"/"//${USER_ID}:${USER_PW}@"}
-git push ${origin_with_pass} master
